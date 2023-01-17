be5ea04826e07eb3d1015db8fb27a25e4949f1bc
luz <lezojeda@gmail.com>
2023-01-17 15:28:04 -0300

    Use typescript and install necessary dependencies

---
+++ server.ts
@@ -0,0 +1,19 @@
+import cors from 'cors'
+import express, { Request, Response, NextFunction} from 'express'
+import morgan from 'morgan'
+
+const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(morgan('dev'))
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
