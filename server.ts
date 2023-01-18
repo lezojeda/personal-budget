0ec1c1dbfcc052d0f4761a68a56b7fd8ca2a358e
luz <lezojeda@gmail.com>
2023-01-18 10:15:28 -0300

    Trying deploy with vercel

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ app.use(morgan('dev'))
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/api', (req: Request, res: Response) => {
     res.send('Hello world')
 })
 
