f7fe146e87df78e9d513a330f9b6ec5cee60a39a
luz <lezojeda@gmail.com>
2023-03-01 12:20:14 -0300

    Same for transaction routes

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -13,14 +13,11 @@ const transactionsRouter = express.Router()
 
 transactionsRouter.use(
   "/:id",
-  param("id", MESSAGES.PATH_ID_MUST_BE_INT).trim().isInt(),
+  param("id", MESSAGES.PATH_ID_MUST_BE_INT).isInt(),
   handleValidationResult,
   checkTransactionAccess
 )
 
-/**
- * TODO: Add optional query param to filter by envelope
- */
 transactionsRouter.get(
   "/",
   query("envelopeId", MESSAGES.QUERY_ID_MUST_BE_INT).isInt().optional(),
@@ -32,7 +29,7 @@ transactionsRouter.get("/:id", getTransactionById)
 
 transactionsRouter.patch(
   "/:id",
-  body("amount", MESSAGES.AMOUNT_REQUIRED).trim().isFloat({ min: 0 }),
+  body("amount", MESSAGES.AMOUNT_REQUIRED).isFloat({ min: 0 }),
   handleValidationResult,
   updateTransactionById
 )
